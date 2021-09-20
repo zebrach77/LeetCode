4cad6d8187ca45d8860f96cad26bdba60648649b
zebrach77 <zebrach77@gmail.com>
2021-09-20 21:40:13 +0300

    Done number 5

--- 5/5.cpp
+++ 5/5.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <stack>
 using namespace std;
 
 
@@ -11,74 +12,84 @@ class Solution
   public:
     bool isPalindrome(string sp)
     {
-      for (int i = 0; i < sp.length()/2; ++i)
+      for (int j = 0; j < sp.length()/2; ++j)
       {
-        if(sp[i] != sp[sp.length()-i-1])
+        if(sp[j] != sp[sp.length()-j-1])
           return false;
       }
       return true;
     }
     string longestPalindrome(string s)
     {
-      string maxRes;
-      int maxLength = 0, ind1 = 0;
-      string res = "", resTemp;
-      for (int i = 0; i < s.length(); ++i)
+      stack<char> st={}, stemp;
+      int ind, maxLength = 0;
+      string res = "", temp;
+      char m1, m2 = s[0];
+      if(isPalindrome(s)) return s;
+      st.push(s[0]);
+      for(int i=1; i<s.length(); ++i)
       {
-        res += s[i];
-        if (isPalindrome(res))
+
+        if(s[i]==m2 || m1==m2)
         {
-          if(res.length() > maxLength)
-          {
-            maxLength = res.length();
-            maxRes = res;
+          ind = i;
+          stemp = st;
+          while(!stemp.empty() && stemp.top() == s[ind] && ind<s.length()){
+            // cout<<"!!!!!!"<<endl;
+            stemp.pop();
+            ++ind;
           }
+          temp = s.substr(2*i-ind, 2*(ind-i));
+          // cout<<temp<<endl;
+          if(temp.length() > maxLength)
+          {
+            maxLength = temp.length();
+            res = temp;
+          }
+
         }
-        else
+        if(s[i]==m1 || m2!=m1)
         {
-          if(ind1)
-          {
-            resTemp = s[ind1-1]+res;
-            if (isPalindrome(resTemp) && resTemp.length() > maxLength)
-            {
-              res = resTemp;
-              --ind1;
-              maxLength = res.length();
-              maxRes = res;
-            }
+          ind = i;
+
+          stemp = st;
+          stemp.pop();
+
+          while(!stemp.empty() && stemp.top() == s[ind]&&ind<s.length()){
+            stemp.pop();
+            ++ind;
           }
-          else
+          temp = s.substr(2*i-ind-1,2*(ind-i)+1);
+          if(temp.length() > maxLength)
           {
-            if(!isPalindrome(res))
-            {
-              for (int j = 1; j < res.length()-maxLength; ++j)
-              {
-                resTemp = res.substr(j, res.length()-1);
-                if (isPalindrome(resTemp) && resTemp.length() > maxLength)
-                {
-                  res = resTemp;
-                  ind1 = j;
-                  maxLength = res.length();
-                  maxRes = res;
-                }
-              }
-            }
+            maxLength = temp.length();
+            res = temp;
           }
+
         }
+        else if(m1==m2)
+        {
+
+        }
+        m1 = m2;
+        m2 = s[i];
+        st.push(s[i]);
       }
-      return maxRes;
+      int i = s.length()-1;
+      if (!i) return s.substr(0,1);
+      if(res.empty()) return s.substr(0, 1);
+      return res;
     }
 };
 
 
 
-
-
-
 int main()
 {
-  string ss = "fsdfghgcfghjdabcdcbad";
+  string ss = "ffffffggggg";
+  // cout<<ss.substr(1, 0)<<endl;
   Solution sol = Solution();
+  // cout<<sol.isPalindrome("aaabaaa")<<endl;
   cout<<sol.longestPalindrome(ss)<<endl;
 }
 //hsdvhibasdfdsbhvuoaijp
